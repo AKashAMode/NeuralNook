319b4526e514b1b1112423fe5ec508d8997cf89c
AKashAMode <akashamode80@gmail.com>
2025-10-24 21:40:33 +0530

    health api created

--- server/index.js
+++ server/index.js
@@ -12,14 +12,31 @@ app.use(cors());
 
 const PORT = 8080 || process.env.PORT;
 
+//database connection
+const connectDB = async () => {
 
-app.get('/getdata',(req, res)=> {
+   try{
+
+      const conn = await mongoose.connect(process.env.MONGODB_URL);
+      if(conn){
+         console.log('MongoDB Connected');
+      }
+   }catch(error){
+     console.log("MongoDB connection error:", error)
+   }
+
+}
+
+
+app.get("/", (req, res)=> {
  res.json({
-    status:true,
-    message:"data fetched successfully"
- })
+   status:true,
+   message:" API Is UP !!!!"
+ });
+
 });
 
 app.listen(PORT, ()=> {
 console.log(`server is running on port ${PORT}`);
+connectDB();
 });
