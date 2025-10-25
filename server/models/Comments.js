f362ac3c1ffbb2ff25c7208e59b729d86a5276a0
AKashAMode <akashamode80@gmail.com>
2025-10-25 12:45:25 +0530

    commit comments  schema details

--- server/models/Comments.js
+++ server/models/Comments.js
@@ -1,7 +1,18 @@
 import {model, Schema} from 'mongoose';
 
 
-const userCommentSchema = ({});
+const userCommentSchema = new Schema(
+
+    {
+      content: {type: String, required: true},
+      user: {type: Schema.Types.ObjectId, ref: "User", required: true},
+      blog: {type: Schema.Types.ObjectId, ref: "Blog", required: true},
+    },
+
+    {
+        timestamps: true,
+    }
+);
 
 
 const Comment = ("Comment", userCommentSchema);
