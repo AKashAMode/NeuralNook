84d17682e622e2e1a8b0c189add458959ade4937
AKashAMode <akashamode80@gmail.com>
2025-10-25 12:33:25 +0530

    commit userschema details

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,18 @@
 import {model, Schema} from 'mongoose';
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+    
+    {
+        name: {type: String, required: true},
+        email: {type:String, required: true},
+        password: {type:String, required: true},
+    },
+
+    {
+        timestamps: true,
+    }
+
+);
 
 
 const User = model("User", userSchema);
