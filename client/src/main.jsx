c80a1dd70f716411f078cad768505e7baefd7253
AKashAMode <akashamode80@gmail.com>
2025-10-27 23:00:56 +0530

    set route for login and signup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from 'react-router'
 import './index.css'
-import App from './App.jsx'
+import Login from './views/Login';
+import Signup from './views/Signup'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/signup' element={<Signup/>} />
+  </Routes>
+  </BrowserRouter>
 )
