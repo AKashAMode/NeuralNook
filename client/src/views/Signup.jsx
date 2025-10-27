93926b6940dce161ad4dd140188cd17a70467d30
AKashAMode <akashamode80@gmail.com>
2025-10-27 22:45:00 +0530

    signup page styling added

--- client/src/views/Signup.jsx
+++ client/src/views/Signup.jsx
@@ -7,37 +7,37 @@ function Signup(){
     return(
         <>
         <div>
-         <h1 className="text-4xl my-4">Signup</h1>
+         <h1 className="text-4xl text-center my-4">Signup</h1>
 
-         <div className="border-2 border-grey-200 py-10 px-15 rounded-md">
+         <div className=" max-w-[400px] mx-auto border-grey-200 py-10 px-15 rounded-md bg-blue-500">
             <input 
             type="name" 
             placeholder="name"
-            className="border p-2 rounded w-full"
+            className="border p-2 rounded w-full mb-4 bg-white"
             />
 
             <input 
             type="email"
             placeholder="email"
-            className="border p-2 rounded w-full"
+            className="border p-2 rounded w-full mb-4 bg-white"
             />
 
             <input 
             type="password"
             placeholder="password"
-            className="border p-2 rounded w-full"
+            className="border p-2 rounded w-full mb-4 bg-white"
             />
 
             <button 
-            className="bg-blue-800"
+            className="bg-blue-600 px-5 py-1 rounded text-white block mx-auto cursor-pointer"
             type="button"
             >
             Signup
             </button>
 
-            <p className="mp-5">
+            <p className="mp-5 text-center p-2 text-white">
                 Already have an account?{" "}
-               <span>Login</span>
+               <span className="text-white cursor-pointer"> Login</span>
             </p>
          </div>
         </div>
