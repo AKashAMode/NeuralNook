38f772fd745221b0aba108d2ec1aae8ddaf3e937
AKashAMode <akashamode80@gmail.com>
2025-10-28 14:57:32 +0530

    commit allblog changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,23 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter, Routes, Route} from 'react-router'
 import './index.css'
 import Login from './views/Login';
-import Signup from './views/Signup'
+import Signup from './views/Signup';
+import AllBlogs  from './views/AllBlogs';
+import EditBlog from './views/EditBlog';
+import NewBlog from './views/NewBlog';
+import ReadBlog from './views/ReadBlog';
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Routes>
     <Route path='/login' element={<Login/>}/>
     <Route path='/signup' element={<Signup/>} />
+    <Route path='/' element={<AllBlogs/>}/>
+    <Route path='/editblog' element={<EditBlog/>}/>
+    <Route path='/newblog' element={<NewBlog/>}/>
+    <Route path='/ReadBlog' element={<ReadBlog/>}/>
+    <Route path='*' element={<h1 className='text-center mt-10'>🫣❗❗❗❗❗❗404 NOT FOUND ❗❗❗❗❗❗🫣</h1>}/>
   </Routes>
   </BrowserRouter>
 )
--- client/src/views/AllBlogs.jsx
+++ client/src/views/AllBlogs.jsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from "react";
+import {getCurrentUser} from './../util';
 
 
 
 function AllBlogs(){
+    const [user, setUser] = useState(null);
+
+    useEffect(()=> {
+        setUser(getCurrentUser);
+    }, [])
 
     return(
         <div>all blog section</div>
-    )
+
+       )
 }
 
 export default AllBlogs;
