dd382af19d546d193da6b2c25bf218b25dc83462
AKashAMode <akashamode80@gmail.com>
2025-10-28 16:43:35 +0530

    new blog page created

---
+++ client/src/constants.js
@@ -0,0 +1,26 @@
+const BLOG_CATEGORIES = 
+[
+  "Food",
+  "Entertainment",
+  "Sports",
+  "Science",
+  "Art",
+  "History",
+  "Politics",
+  "Environment",
+  "Business",
+  "Culture",
+  "Fashion",
+  "Technology",
+  "Health",
+  "Travel",
+  "Finance",
+  "Education",
+  "Lifestyle",
+  "Photography",
+  "DIY",
+  "Parenting",
+  "Other",
+];
+
+export { BLOG_CATEGORIES };
--- client/src/views/NewBlog.jsx
+++ client/src/views/NewBlog.jsx
@@ -1,13 +1,38 @@
 import MarkdownEditor from "@uiw/react-markdown-editor";
-
+import { useState } from "react";
+import { BLOG_CATEGORIES } from './../constants';
+import { getCurrentUser } from "../util";
+import {ToastContainer, toast} from 'react-toastify';
 
 
 function NewBlog(){
+    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState(BLOG_CATEGORIES[0]);
 
 
     return(
-        <div>
-          <h1>New Blog</h1>  
+        <div className="container mx-auto p-5">
+
+          <h1>New Blog</h1>
+
+          <input type="text"
+          placeholder="Blog Title"
+          className="border p-2 w-full my-4 rounded"
+
+          /> 
+
+          <select 
+          className="border p-2  my-5 rounded cursor-pointer"
+
+          >
+          {BLOG_CATEGORIES.map((categ)=> {
+            return(
+                <option key={categ} value={categ}>{categ}</option>
+            )
+          })}
+          </select>
+
           <MarkdownEditor
           value={""}
           onChange={(value)=> {
@@ -15,6 +40,10 @@ function NewBlog(){
           }}
           height="500px"
           />
+
+          <button className="text-white bg-blue-700 px-5 py-2 mt-5 rounded cursor-pointer">
+            Save Blog
+            </button>
         </div>
     )
 }
