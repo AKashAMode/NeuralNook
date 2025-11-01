06218689ad5e9ee8885dd266a2aa9a116c82f562
AKashAMode <akashamode80@gmail.com>
2025-11-01 19:47:10 +0530

    feat: real blog by slug

--- client/src/views/ReadBlog.jsx
+++ client/src/views/ReadBlog.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from 'axios';
 import { useParams } from "react-router";
-
+import MarkdownEditor from "@uiw/react-markdown-editor";
 
 function ReadBlog(){
     const { slug } = useParams();
@@ -17,16 +17,39 @@ function ReadBlog(){
 
 
     useEffect(()=> {
+        document.documentElement.setAttribute("data-color-mode", "light");
        fetchBlog();
     }, [])
 
     return(
 
-        <div>
-        <h1>read blog</h1>
-        <h1>{blog.title}</h1>
+        <div className="container p-4 mx-auto">
+        <h1 className="text-2xl font-bold md-4">{blog.title}</h1>
+
+        <p>Published On: {" "}
+            {new Date(blog.publishedAt || blog.updatedAt).toLocaleDateString()}
+        </p>
+        
+        <div className="flex items-center md-4">
+            <span>
+                {blog.category}
+                </span>
+
+                <div className="flex items-center gap-4 my-2 ml-14">
+                    <div className="flex items-center justify-center font-semibold w-[50px] h-[50px] bg-orange-300 text-center text-white rounded-full text-3xl">
+                        {blog?.author?.name?.substring(0,1)}
+                    </div>
+
+                    <div>
+                        <p>{blog?.author?.name}</p>
+                        <p>{blog?.author?.email}</p>
+                    </div>
+                </div>
+
         </div>
-    )
+          <MarkdownEditor.Markdown source={blog.content}/>
+        </div>
+    );
 }
 
 export default ReadBlog;
